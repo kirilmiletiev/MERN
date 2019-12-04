c310e7db37cb52e4ddf1920acecfc2144aa139cf
KirilMiletiev <kirilmiletiev@gmail.com>
2019-12-05 01:27:12 +0200

    Fix edit-items.js

--- src/client/mern/src/components/edit-items.js
+++ src/client/mern/src/components/edit-items.js
@@ -94,59 +94,42 @@ export default class EditItems extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Edit Item Log</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Username: </label>
-                        <select ref="userInput"
-                            required
-                            className="form-control"
-                            value={this.state.username}
-                            onChange={this.onChangeUsername}>
-                            {
-                                this.state.users.map(function (user) {
-                                    return <option
-                                        key={user}
-                                        value={user}>{user}
-                                    </option>;
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label>Description: </label>
-                        <input type="text"
-                            required
-                            className="form-control"
-                            value={this.state.description}
-                            onChange={this.onChangeDescription}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Duration (in minutes): </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={this.state.duration}
-                            onChange={this.onChangeDuration}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Date: </label>
-                        <div>
-                            <DatePicker
-                                selected={this.state.date}
-                                onChange={this.onChangeDate}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <input type="submit" value="Edit Item Log" className="btn btn-primary" />
-                    </div>
-                </form>
-            </div>
+            this.getHtml()
         )
+    }
+
+    getHtml() {
+        return <div>
+            <h3>Edit Item Log</h3>
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <select ref="userInput" required className="form-control" value={this.state.username} onChange={this.onChangeUsername}>
+                        {this.state.users.map(function (user) {
+                            return <option key={user} value={user}>{user}
+                            </option>;
+                        })}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>Description: </label>
+                    <input type="text" required className="form-control" value={this.state.description} onChange={this.onChangeDescription} />
+                </div>
+                <div className="form-group">
+                    <label>Duration (in minutes): </label>
+                    <input type="text" className="form-control" value={this.state.duration} onChange={this.onChangeDuration} />
+                </div>
+                <div className="form-group">
+                    <label>Date: </label>
+                    <div>
+                        <DatePicker selected={this.state.date} onChange={this.onChangeDate} />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <input type="submit" value="Edit Item Log" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>;
     }
 }
